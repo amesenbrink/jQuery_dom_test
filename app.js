d2cf66f2ad7c6c442a00e2e62e7d797865b7cccf
Adam Mesenbrink <amesenbrink@gmail.com>
2015-02-03 11:22:53 -0600

    corrected .js loading order
    app.js was loading prior to the DOM, so it could not change elements
    that were yet undefined.

--- app.js
+++ app.js
@@ -10,10 +10,11 @@ var family1, family2, bruce, madison, hunter;
 
 family1 = $('#family1');
 family2 = $('<div id="family2"><h1>family2</h1></div>');
-bruce = $('<div id="bruce"><h1>bruce</h1></div>');
-madison = $('<div id="madison"><h1>madison</h1></div>');
-hunter = $('<div id="hunter"><h1>hunter</h1></div>');
+bruce = $('<div id="bruce"><h2>bruce</h2></div>');
+madison = $('<div id="madison"><h3>madison</h3></div>');
+hunter = $('<div id="hunter"><h3>hunter</h3></div>');
 
 family2.insertAfter(family1);
-
-
+family2.append(bruce);
+bruce.append(madison);
+bruce.append(hunter);
